2f2f73fa00c4823559bfabbcccb888f665dea650
danrae pray <danrae.pray@outlook.com>
2023-03-26 14:32:20 -0500

    test is ready!

---
+++ Assets/PsyanimTest/PsyanimTestCanvas.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PsyanimTestCanvas : MonoBehaviour
+{
+    public List<string> scenes = new List<string>();
+
+    public TextMeshProUGUI sceneNameTextField;
+
+    private int _currentSceneIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.DontDestroyOnLoad(this.gameObject);
+
+        handleSceneLoaded(SceneManager.GetActiveScene(), LoadSceneMode.Single);
+
+        SceneManager.sceneLoaded += this.handleSceneLoaded;
+    }
+
+    void handleSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        sceneNameTextField.text = scenes[_currentSceneIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            _currentSceneIndex++;
+
+            if (_currentSceneIndex == scenes.Count)
+            {
+                _currentSceneIndex = 0;
+            }
+
+            SceneManager.LoadScene(scenes[_currentSceneIndex]);
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            _currentSceneIndex--;
+
+            if (_currentSceneIndex < 0)
+            {
+                _currentSceneIndex = scenes.Count - 1;
+            }
+
+            SceneManager.LoadScene(scenes[_currentSceneIndex]);
+        }
+    }
+}
